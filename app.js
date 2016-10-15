b38744cac1419bbb43a3c6bd9242a2da21b10195
rajashekar <rajashekarkhallel@gmail.com>
2016-10-15 11:58:17 +0530

    commented unnecessary  code

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = express();
 
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
+//var cool = require('cool-ascii-faces');
+//var express = require('express');
+//var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+//app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
